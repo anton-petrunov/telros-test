d0ecb62b4f1ba1c7c2b3a77c48d9e70aa33223f4
Anton Petrunov <a.n.tone@mail.ru>
2022-10-17 21:44:37 +0300

    Initial commit

---
+++ src/main/java/io/github/anton_petrunov/telros_test/TelrosTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.anton_petrunov.telros_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelrosTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelrosTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/anton_petrunov/telros_test/TelrosTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.anton_petrunov.telros_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelrosTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
