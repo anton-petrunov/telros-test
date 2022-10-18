a9e700a21e4d361973e3f283f54a90a6c72051e8
Anton Petrunov <a.n.tone@mail.ru>
2022-10-18 22:50:48 +0300

    Set HSQLDB, schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+DELETE
+FROM USERS;
+
+INSERT INTO USERS (LAST_NAME, FIRST_NAME, PATRONYMIC, BIRTHDAY, EMAIL, PHONE)
+VALUES ('petrunov', 'anton', 'nikolaevich', '1984-11-17', 'petrunov.ru@gmail.com', '+79312211019'),
+       ('Sochnev', 'Ignat', 'Ivanovich', '2000-10-30', 'ingat@fanat.ru', '3333333');
