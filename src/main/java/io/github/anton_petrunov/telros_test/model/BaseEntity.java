3ba15c3b7f86fae4cf9ae3186730d3b4850a880a
Anton Petrunov <a.n.tone@mail.ru>
2022-10-20 08:23:08 +0300

    Update model, add BaseEntity

---
+++ src/main/java/io/github/anton_petrunov/telros_test/model/BaseEntity.java
@@ -0,0 +1,45 @@
+package io.github.anton_petrunov.telros_test.model;
+
+import lombok.*;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.util.ProxyUtils;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Access(AccessType.FIELD)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public abstract class BaseEntity implements Persistable<Integer> {
+
+    @Id
+    protected Integer id;
+
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !getClass().equals(ProxyUtils.getUserClass(obj))) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) obj;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
