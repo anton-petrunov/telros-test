c9579bf4efa9bd42753486b6d95678544980bc8a
Anton Petrunov <a.n.tone@mail.ru>
2022-10-20 15:31:35 +0300

    Add util and ValidationUtil

---
+++ src/main/java/io/github/anton_petrunov/telros_test/util/ValidationUtil.java
@@ -0,0 +1,37 @@
+package io.github.anton_petrunov.telros_test.util;
+
+import io.github.anton_petrunov.telros_test.error.IllegalRequestDataException;
+import io.github.anton_petrunov.telros_test.error.NotFoundException;
+import io.github.anton_petrunov.telros_test.model.BaseEntity;
+
+public class ValidationUtil {
+
+    public static void checkNew(BaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(BaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be with id=" + id);
+        }
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+}
